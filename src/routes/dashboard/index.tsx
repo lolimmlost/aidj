7a649233bf42190cde01d354f65e5c86918f2b6c
juan <johnoflol@gmail.com>
2025-09-06 18:52:19 -0700

    added config settings link to index page

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/dashboard/")({
   component: DashboardIndex,
@@ -11,6 +11,11 @@ function DashboardIndex() {
       <pre className="bg-card text-card-foreground rounded-md border p-1">
         routes/dashboard/index.tsx
       </pre>
+      <div className="mt-2">
+        <Link to="/config" className="underline">
+          Service Configuration
+        </Link>
+      </div>
     </div>
   );
 }
